aba9c7c32143919b0f536fdb23963e0002ed3fc9
rubinasatyal <rubinasatyal@gmail.com>
2017-02-06 15:32:26 -0500

    assignment2

---
+++ main.js
@@ -0,0 +1,6 @@
+var person=prompt("enter your value", " "); 
+alert("There are"+' '+ person.length + ' '+ "characters in the word"); 
+alert(person.toLowerCase()); 
+alert(person.toUpperCase()); 
+alert("I memorized his monologue" + ' '+ person); 
+alert(person.substring(1,4)); 
